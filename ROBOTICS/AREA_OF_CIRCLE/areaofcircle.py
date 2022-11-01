a5bbf4e3b35faef56c381984d408fecbb32f44b2
gpdzvapatsva <gpdzvapatsva@gmail.com>
2022-11-01 13:37:23 +0200

    topic 4 changes

---
+++ ROBOTICS/AREA_OF_CIRCLE/areaofcircle.py
@@ -0,0 +1,7 @@
+#declaring variables and constants.
+radius=int(input("Please enter the radius of the circle"))
+pi=3.14
+#processing
+area_of_circle=pi*radius*radius
+#displaying the area of the circle
+print(f"The area of a circle is {area_of_circle}")
