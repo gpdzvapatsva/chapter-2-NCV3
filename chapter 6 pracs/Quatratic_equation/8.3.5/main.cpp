de5e8ac0ae8825e629a2cf7072f97b89e3810154
godwin <gpdzvapatsva@gmail.com>
2023-02-25 15:16:40 +0200

    8.3.6

---
+++ chapter 6 pracs/Quatratic_equation/8.3.5/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+bool result;
+result=(65>'#');
+ cout <<boolalpha<< result << endl;
+return 0;
+}
---
+++ chapter 6 pracs/Quatratic_equation/8.4/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int result;
+    result=(65>'H')&& 0b11;
+    cout <<result << endl;
+    return 0;
+}
