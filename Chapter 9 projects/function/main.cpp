094bbfbb8f75801dee4446124bce45a16bab73c5
godwin <gpdzvapatsva@gmail.com>
2023-04-10 14:46:21 +0200

    ch10

---
+++ Chapter 9 projects/function/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//declaring afunction without parameters
+void userDetails(){
+cout <<"I am learning about C++ function";
+}
+
+int main()
+{//calling a function
+   userDetails();
+    return 0;
+}
