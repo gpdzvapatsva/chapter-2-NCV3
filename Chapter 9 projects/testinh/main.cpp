aaf9c2b9e3068f19a98f3237d927e17b625c37fc
godwin <gpdzvapatsva@gmail.com>
2023-05-17 23:03:31 +0200

    latest

---
+++ Chapter 9 projects/testinh/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#include <string>
+
+string str = "42";
+int num = stoi(str); // Converts the string to an int  return 0;
+cout<<num+num<<endl;
+}
